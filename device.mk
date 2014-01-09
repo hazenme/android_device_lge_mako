7eec3950d0efd06d32f9ca1ddafc776c82beb497
Tim Clark <tim.clark.82@gmail.com>
2014-01-09 11:50:48 -0700

    mako: Copy GoogleDialer.apk to /system/priv-app on HAZE builds
    AOSP Dialer has been removed in favor of a fully functional Google Dialer in HAZE builds
    
    Change-Id: I66daa919afb41e55d5d178ff97feabe33d9ea8f0

--- device.mk
+++ device.mk
@@ -293,3 +293,7 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 $(call inherit-product, frameworks/base/data/sounds/AudioPackage10.mk)
 
 $(call inherit-product, hardware/qcom/msm8960/msm8960.mk)
+
+# Google Dialer
+PRODUCT_COPY_FILES += \
+    prebuilt-apks/GoogleDialer.apk:system/priv-app/GoogleDialer.apk
